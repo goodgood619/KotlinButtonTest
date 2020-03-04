1da6aa4d1f52b1f65262bc16d1e77274a8e19cda
goodgood619 <gktgnjftm@naver.com>
2020-03-04 16:16:51 +0900

    branch변경후 testcommit

--- app/src/main/java/com/example/kotlinbuttontest/RetrofitService.kt
+++ app/src/main/java/com/example/kotlinbuttontest/RetrofitService.kt
@@ -15,6 +15,8 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+
+
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File

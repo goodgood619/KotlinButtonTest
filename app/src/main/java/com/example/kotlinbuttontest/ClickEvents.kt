0d438fd128e87f28d0bae764a0f50b4c0e3007d9
Jay-lol <krgeo@naver.com>
2020-03-10 16:38:56 +0900

    비동기화 해보기

---
+++ app/src/main/java/com/example/kotlinbuttontest/ClickEvents.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinbuttontest
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedInputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+class ClickEvents : AppCompatActivity(){
+
+
+}
